7b5254ed67c051717696f797e92c4f0896a2b3a7
summerTaha <136254931+summerTaha@users.noreply.github.com>
2023-06-11 22:41:15 +0300

    Create CountryController.cs

---
+++ MVC_Example/Controllers/CountryController.cs
@@ -0,0 +1,12 @@
+﻿using BusinessLayer.Services;
+using MVC_Example.Dtos;
+
+namespace MVC_Example.Controllers
+{
+    public class CountryController : BaseCrudController<CountryDto>
+    {
+        public CountryController(ICrudService<CountryDto> crudService) : base(crudService)
+        {
+        }
+    }
+}
